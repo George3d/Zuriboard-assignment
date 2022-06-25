110a32860bc56a67e315daaf815e5363169f20a0
IKECHI GEORGE ONUOBIA <57946648+George3d@users.noreply.github.com>
2022-06-25 22:24:23 +0100

    The new files are a simple calculator in js

---
+++ Calculator.js
@@ -0,0 +1,16 @@
+//My first calculator
+//Input any variable for figure1 and figure2
+let figure1 = 8;
+let figure2 = 5;
+sum = figure1 + figure2;
+subtraction = figure1 - figure2;
+multiplication = figure1 * figure2;
+division = figure1 / figure2;
+console.log('The sum of figure1 and figure2 is');
+console.log(sum);
+console.log('The subtraction between figure1 and figure2 is');
+console.log(subtraction);
+console.log('The multiplication between figure1 and figure2 is');
+console.log(multiplication);
+console.log('The divsion between figure1 and figure2 is');
+console.log(division);
